78091a3bcf150ae2eb54d87b011991507265bfec
Calum <calum.rogers@ntlworld.com>
2022-02-10 14:25:39 +0000

    second commit custom GameGrub card component

--- App.js
+++ App.js
@@ -1,21 +1,12 @@
 import React from "react";
-import { Text, Image, View, StyleSheet, ImageBackground } from 'react-native';
+import { View, StyleSheet, } from 'react-native';
+import Card from "./src/components/GameGrubCard";
+import foods from './assets/data/foods';
 
 const App = () => {
   return (
     <View style={styles.pageContainer}>
-      <View style={styles.card}>
-        <ImageBackground
-          source={{
-            uri: 'https://www.japan.go.jp/kizuna/_src/7989285/sushi_robots_01.jpg?v=1643014433922'
-          }}
-          style={styles.image}>
-          <View style={styles.cardInner}>
-            <Text style={styles.name}>Sushi</Text>
-            <Text style={styles.bio} >Sushi is a traditional Japanese dish of prepared vinegared rice, usually with some sugar and salt, accompanied by a variety of ingredients, such as seafood, often raw, and vegetables.</Text>
-          </View>
-        </ImageBackground>
-      </View>
+      <Card food={foods[3]}/>
     </View>
 
   );
@@ -27,50 +18,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     flex: 1
   },
-  card: {
-    width: '95%',
-    height: '70%',
-    borderRadius: 10,
 
-    shadowColor: "#000",
-    shadowOffset: {
-      width: 0,
-      height: 5,
-    },
-    shadowOpacity: 0.36,
-    shadowRadius: 6.68,
-
-    elevation: 11,
-
-
-  },
-  image:
-  {
-    width: '100%',
-    height: '100%',
-    borderRadius: 10,
-    overflow: 'hidden',
-
-    justifyContent: 'flex-end',
-
-  },
-  cardInner: {
-    padding:10,
-    
-  },
-
-
-  name: {
-    fontSize: 30,
-    color: 'white',
-    fontWeight: 'bold',
-    
-  },
-  bio: {
-    fontSize: 18,
-    color: 'white',
-    lineHeight: 25,
-  }
 
 })
 export default App;
---
+++ assets/data/foods.js
@@ -0,0 +1,37 @@
+export default [
+  {
+    id: '1',
+    name: 'Sushi',
+    image:
+    'https://upload.wikimedia.org/wikipedia/commons/thumb/9/9f/California_Sushi_%2826571101885%29.jpg/1024px-California_Sushi_%2826571101885%29.jpg',
+    bio: 'Sushi is a traditional Japanese dish of prepared vinegared rice, usually with some sugar and salt, accompanied by a variety of ingredients, such as seafood, often raw, and vegetables.',
+  },
+  {
+    id: '2',
+    name: 'Burger',
+    image:
+      'https://static01.nyt.com/images/2021/05/17/dining/kc-korean-bulgogi-burger/kc-korean-bulgogi-burger-mediumSquareAt3X.jpg',
+    bio: 'A hamburger is a food, which in American English is considered a sandwich, consisting of one or more cooked patties—usually ground meat, typically beef—placed inside a sliced bread roll or bun.',
+  },
+  {
+    id: '3',
+    name: 'Pizza',
+    image:
+      'https://www.recipetineats.com/wp-content/uploads/2020/05/Pizza-Crust-without-yeast_5-SQ.jpg',
+    bio: 'Pizza is a dish of Italian origin consisting of a usually round, flat base of leavened wheat-based dough topped with tomatoes, cheese, and often various other ingredients',
+  },
+  {
+    id: '4',
+    name: 'Chocolate Cake',
+    image:
+      'https://food-images.files.bbci.co.uk/food/recipes/easy_chocolate_cake_31070_16x9.jpg',
+    bio: 'Chocolate cake or chocolate gâteau is a cake flavored with melted chocolate, cocoa powder, or both.',
+  },
+  {
+    id: '5',
+    name: 'Tacos',
+    image:
+      'https://assets.tmecosys.com/image/upload/t_web767x639/img/recipe/ras/Assets/fa63faff-b39d-4464-9e7c-c57dbc9a533d/Derivates/28453038-6031-43f9-9b9e-d586f5b7ea5c.jpg',
+    bio: 'A taco is a traditional Mexican dish consisting of a small hand-sized corn or wheat tortilla topped with a filling. The tortilla is then folded around the filling and eaten by hand.',
+  },
+];
